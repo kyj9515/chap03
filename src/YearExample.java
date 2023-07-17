709e95d1ec494bd6561206297fd305ea0bc1ee0a
kyj9515 <ydh981@naver.com>
2023-07-17 18:05:35 +0900

    ...

--- src/YearExample.java
+++ src/YearExample.java
@@ -1,4 +1,13 @@
 
 public class YearExample {
+	public void main(String[] args) {
+		double oneYear = 365.2422;
+		double tenYear = oneYear * 100;
+		
+		System.out.println(tenYear);
+		
+		System.out.println(366*25 + 365*75);
+		
+	}
 
 }
